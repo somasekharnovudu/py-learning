25a2e09b45a74a2a371e03430e47efd77336275f
Novudu Soma Sekhar <novudu.somasekhar702@gmail.com>
2025-01-29 00:40:32 +0530

    Added operators

--- 101_02_operators.py
+++ 101_02_operators.py
@@ -5,6 +5,7 @@ num1 = 10
 num2 = 20
 print('====== +')
 print(num1+num2)
+# can also use like num+=2
 
 print('====== -')
 print(num1-num2)
@@ -31,3 +32,27 @@ print(name1+' '+name2)
 
 print('==== string repetition')
 print(name1*5)
+
+# compare operators
+
+# ==, != ,>=, <=, > , < is, in
+
+print('===== =')
+print(1 == 1)
+
+print('===== !=')
+print(1 != 2)
+
+print('===== >=')
+print(1 >= 1)
+
+print('===== <=')
+print(1 == 1)
+
+print('===== is')  # compares reference of objects
+list = [1, 2, 3]
+list2 = list
+print(list is list2)
+
+print('===== in')  # checks existance of element in list
+print(1 in [1, 2, 3])
